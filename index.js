805dedeb68a2c43be9f22f479a5bda6c4644988b
OrestisP <64751688+OrestisP@users.noreply.github.com>
2021-04-07 04:13:56 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function rollDice(){
+
+  var dice = Math.random() * 6 + 1; // or: Math.floor(Math.random()*6+1);
+  result = Math.floor(dice);
+  return result;
+}
+
+var randomNumber1 = rollDice();
+var randomNumber2 = rollDice();
+var diceImg = ["images/dice1.png", "images/dice2.png", "images/dice3.png",
+  "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var winner = document.querySelector("h1");
+// how to get the attribute src of the tag img:
+// document.querySelector("img").getAttribute("src");
+// in order to change the value of the attribute:
+// document.querySelector("img").setAttribute("src", "<here comes the new value>");
+
+for (var i = 0; i < diceImg.length; i++) {
+
+  if (randomNumber1 == i) {
+    document.querySelector("img.img1").setAttribute("src", diceImg[i]);
+    }
+  if (randomNumber2 == i) {
+    document.querySelector("img.img2").setAttribute("src", diceImg[i]);
+    }
+}
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "😎 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  winner.textContent = "🤩 Player 2 Wins!";
+} else {
+  winner.textContent = "Draw 🤮";
+}
